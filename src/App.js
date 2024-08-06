4e8ddda64e5612385a13519b2dcf20effaa99d75
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2024-08-06 13:49:56 -0300

    Melhorias no front

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home/Home';
 import Itens from './pagesVendas/Itens';
 import Vendas from './pagesVendas/Vendas';
-import Pedidos from './pagesVendas/Pedidos';
 import RelatorioVendas from './pagesVendas/RelatorioVendas';
 import GeradorRecibos from './geradorRecibos/GeradorRecibos'
 import NovoFinanceiro from './financeiro/NovoFinanceiro';
@@ -15,7 +14,6 @@ const App = () => {
                 <Route exact path="/" element={<Home />} />
                 <Route path="/itens" element={<Itens />} />
                 <Route path="/vendas" element={<Vendas />} />
-                <Route path="/pedidos" element={<Pedidos />} />
                 <Route path='/relatorio'element={<RelatorioVendas/>} />
                 <Route path='/geradorRecibos' element={<GeradorRecibos />} />
                 <Route path='/novoFinanceiro' element={<NovoFinanceiro />}/>
