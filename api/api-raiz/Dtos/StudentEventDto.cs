b25b062f4690b596e79f532a43c3d96c7a56c1f2
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2024-08-14 14:45:41 -0300

    Update StudentEventDto.cs

--- api/api-raiz/Dtos/StudentEventDto.cs
+++ api/api-raiz/Dtos/StudentEventDto.cs
@@ -6,7 +6,8 @@ public class StudentEventDto
 {
     public int EventId { get; set; }
     public int Registration { get; set; }
-    public string Name { get; set; }
-    public string Responsible { get; set; }
+    public string? Name { get; set; }
+    public string? Responsible { get; set; }
     public int GroupId { get; set; }
 }
+
